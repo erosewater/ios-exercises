b980462e2932e29fd28277771121a4264d7234fa
David Krasnove <krasnove@mac.com>
2014-11-04 11:41:18 -0800

    Completed if, else, ternery

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -12,23 +12,45 @@
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
-
-    /* WORK HERE */
+    NSInteger gum = 5;
+    NSInteger apple = 6;
+    NSInteger appleComputer = 1000;
+    NSInteger bigApple = 1000000000;
     
+  // Tried this a few ways.  The case I didn't handle is numbers in between.  Wasn't sure how to do these without creating a long expression, however, I am still looking into this.
+    
+    if (dollars < gum) {
+        itemToReturn = @"get out of my store";
+     }
+    else if (dollars == gum) {
+        itemToReturn = @"have some gum";
+     }
+    else if (dollars == apple) {
+        itemToReturn = @"have an apple";
+     }
+
+    else if (dollars == appleComputer) {
+        itemToReturn = @"have an Apple computer";
+     }
+    else if (dollars == bigApple) {
+        itemToReturn = @"have The Big Apple";
+   } else {
+       itemToReturn = @"buy whatever you like up to %ld", (long)dollars;
+         }
+
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
-    /* WORK HERE */
+  
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    NSUInteger finalCost = self.getsDiscount ? (cost *=.75) : (cost = 24);
     
-    return cost;
+    
+    return finalCost;
 }
 
 @end
