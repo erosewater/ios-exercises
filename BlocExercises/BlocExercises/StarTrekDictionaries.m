a7cf3b7dcbe61fbcf5fcf8438d43debfa54dfb23
David Krasnove <krasnove@mac.com>
2014-11-12 10:19:34 -0800

    Completed dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -11,18 +11,43 @@
 @implementation StarTrekDictionaries
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @"";
-}
+   
+    
+    NSString *favoriteDrinkForStarTrekCharacterDictionary = [characterDictionary objectForKey:@"favorite drink"];
+    return favoriteDrinkForStarTrekCharacterDictionary;
+    }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    /* WORK HERE */
-    return @[];
+   // I'd like to find a better way to do this.  I'm still confused about an array of dictionary items.  What I would like to do is iterate over the array, pull out each of the objects that meet the key and add them to a new array.  Each dictionary is an object in the array.  This array holds two dictionaries.
+   
+    
+    
+    NSMutableDictionary *workingDict = [charactersArray[0] mutableCopy];
+    NSMutableDictionary *workingDict2 = [charactersArray[1] mutableCopy];
+    
+    
+    NSString *drink1 = [workingDict objectForKey:@"favorite drink"];
+    NSString *drink2 = [workingDict2 objectForKey:@"favorite drink"];
+    
+    NSArray *arrayOfFavoriteDrinksForStarTrekCharacters = [NSArray arrayWithObjects: drink1
+                                                           , drink2
+                                                           , nil];
+    
+    
+    
+    return arrayOfFavoriteDrinksForStarTrekCharacters;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @{};
-}
+   
+   
+    NSString *newQuote = @"Shut up, Wesley";
+   
+    NSMutableDictionary *newDictionaryWithQuoteAddedToStarTrekCharacterDictionary = [characterDictionary mutableCopy];
+    [newDictionaryWithQuoteAddedToStarTrekCharacterDictionary setObject:newQuote forKey:@"quote"];
+    
+    return newDictionaryWithQuoteAddedToStarTrekCharacterDictionary;
+    }
+
 
 @end
